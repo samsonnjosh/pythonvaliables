48079817edc09cfcd18ac44bf26f52d1be518810
mamboleo254 <sancristma@gmail.com>
2021-01-27 15:55:24 +0300

    update commit

---
+++ variables.py
@@ -0,0 +1,24 @@
+# An integer assignment
+age = 45
+print (age)
+# A string
+name = "samson mwangi"
+print(name)
+# A floating number
+myFee = 2000.09
+print(myFee)
+# A double number
+myMoney = 22/7
+print(myMoney)
+a=b=c=10
+print(a)
+print(b)
+print(c)
+a,b,c = 1,20.2,"python programming"
+print(a)
+print(b)
+print(c)
+
+
+
+
